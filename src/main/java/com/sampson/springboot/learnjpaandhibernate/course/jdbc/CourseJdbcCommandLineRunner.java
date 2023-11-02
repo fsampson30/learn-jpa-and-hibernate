1f1ab51d9fc06971c97dcdad03f0c7b0e52b2494
fsampson30 <fsampson30@gmail.com>
2023-11-02 14:57:32 -0300

    Creted method to delete by id.

--- src/main/java/com/sampson/springboot/learnjpaandhibernate/course/jdbc/CourseJdbcCommandLineRunner.java
+++ src/main/java/com/sampson/springboot/learnjpaandhibernate/course/jdbc/CourseJdbcCommandLineRunner.java
@@ -1,6 +1,7 @@
 package com.sampson.springboot.learnjpaandhibernate.course.jdbc;
 
 
+import com.sampson.springboot.learnjpaandhibernate.course.Course;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -13,6 +14,10 @@ public class CourseJdbcCommandLineRunner implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        repository.insert();
+        repository.insert(new Course(1,"Learn AWS","Sampson"));
+        repository.insert(new Course(2,"Learn Docker","Sampson"));
+        repository.insert(new Course(3,"Learn Azure","Sampson"));
+        repository.insert(new Course(4,"Learn DevOps","Sampson"));
+        repository.delete(4);
     }
 }
