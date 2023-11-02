4a5b622f952bdc77d41becb1fef2da6df5d60944
fsampson30 <fsampson30@gmail.com>
2023-11-02 16:12:33 -0300

    Created a Course Repository using Spring Data JPA.

--- src/main/java/com/sampson/springboot/learnjpaandhibernate/course/CourseCommandLineRunner.java
+++ src/main/java/com/sampson/springboot/learnjpaandhibernate/course/CourseCommandLineRunner.java
@@ -2,6 +2,7 @@ package com.sampson.springboot.learnjpaandhibernate.course;
 
 
 import com.sampson.springboot.learnjpaandhibernate.course.jpa.CourseJpaRepository;
+import com.sampson.springboot.learnjpaandhibernate.course.springdatajpa.CourseSpringDataJpaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,16 +13,29 @@ public class CourseCommandLineRunner implements CommandLineRunner {
 //    @Autowired
 //    private CourseJdbcRepository repository;
 
+//    @Autowired
+//    private CourseJpaRepository repository;
+
     @Autowired
-    private CourseJpaRepository repository;
+    private CourseSpringDataJpaRepository repository;
 
     @Override
     public void run(String... args) throws Exception {
-        repository.insert(new Course(1, "Learn AWS", "Sampson"));
-        repository.insert(new Course(2, "Learn Docker", "Sampson"));
-        repository.insert(new Course(3, "Learn Azure", "Sampson"));
-        repository.insert(new Course(4, "Learn DevOps", "Sampson"));
-        repository.deleteById(4);
-        System.out.println(repository.findById(1));
+//        repository.insert(new Course(1, "Learn AWS", "Sampson"));
+//        repository.insert(new Course(2, "Learn Docker", "Sampson"));
+//        repository.insert(new Course(3, "Learn Azure", "Sampson"));
+//        repository.insert(new Course(4, "Learn DevOps", "Sampson"));
+//        repository.deleteById(4);
+//        System.out.println(repository.findById(1));
+
+        repository.save(new Course(1, "Learn AWS", "Sampson"));
+        repository.save(new Course(2, "Learn Docker", "Sampson"));
+        repository.save(new Course(3, "Learn Azure", "Sampson"));
+        repository.save(new Course(4, "Learn DevOps", "Sampson"));
+
+        repository.deleteById(4L);
+
+        System.out.println(repository.findById(1L));
+
     }
 }
---
+++ src/main/java/com/sampson/springboot/learnjpaandhibernate/course/springdatajpa/CourseSpringDataJpaRepository.java
@@ -0,0 +1,9 @@
+package com.sampson.springboot.learnjpaandhibernate.course.springdatajpa;
+
+import com.sampson.springboot.learnjpaandhibernate.course.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CourseSpringDataJpaRepository extends JpaRepository<Course, Long> {
+}
